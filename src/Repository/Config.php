f81e3a1d62f778e607edbd5ee188d7727f584dd3
Basil Suter <git@nadar.io>
2024-12-16 20:03:27 +0000

    refactor Config class to be abstract and define required methods for subclasses

--- src/Repository/Config.php
+++ src/Repository/Config.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Darken\Repository;
 
+use DI\Container;
+use DI\ContainerBuilder;
 use Throwable;
 use Yiisoft\Files\FileHelper;
 
-class Config
+abstract class Config
 {
     public function __construct(private readonly string $rootDirectoryPath, private readonly bool $loadDotEnv = true)
     {
@@ -15,6 +17,15 @@ class Config
             $this->loadEnvFile();
         }
     }
+    
+    abstract public function getDebugMode(): bool;
+
+    abstract public function getBuildOutputFolder(): string;
+
+    abstract public function getPagesFolder(): string;
+
+    abstract public function getBuildOutputNamespace(): string;
+
 
     public function loadEnvFile(): void
     {
@@ -38,25 +49,7 @@ class Config
         return FileHelper::normalizePath($this->rootDirectoryPath);
     }
 
-    public function getDebugMode(): bool
-    {
-        return (bool) self::env('DARKEN_DEBUG', false);
-    }
-
-    public function getBuildOutputFolder(): string
-    {
-        return $this->getRootDirectoryPath() . DIRECTORY_SEPARATOR . self::env('DARKEN_BUILD_OUTPUT_FOLDER', '.build');
-    }
-
-    public function getPagesFolder(): string
-    {
-        return $this->getRootDirectoryPath() . DIRECTORY_SEPARATOR . self::env('DARKEN_PAGES_FOLDER', 'pages');
-    }
-
-    public function getBuildOutputNamespace(): string
-    {
-        return self::env('DARKEN_BUILD_OUTPUT_NAMESPACE', 'Build');
-    }
+    
 
     public function getBuildingFolders(): array
     {
@@ -66,7 +59,7 @@ class Config
         ];
     }
 
-    public static function env(string $name, string|bool|int $default = ''): string|bool|int
+    public function env(string $name, string|bool|int $default = ''): string|bool|int
     {
         return getenv($name) ?: $default;
     }
