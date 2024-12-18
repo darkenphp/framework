80129e7f7c047a6c68e0fadecde1b9615b54760d
Basil Suter <git@nadar.io>
2024-12-18 19:28:58 +0000

    remove simplified_null_return rule and update PHPStan level to 3 with error ignores

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -23,7 +23,6 @@ return $config->setRules([
     'return_assignment' => true,
     'semicolon_after_instruction' => true,
     'short_scalar_cast' => true,
-    'simplified_null_return' => true,
     'blank_lines_before_namespace' => true,
     'single_class_element_per_statement' => true,
     'single_line_comment_style' => true,
