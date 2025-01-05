ece130ab16f77ca32c8ec54e820625a7d9f5f44a
Basil Suter <git@nadar.io>
2025-01-05 13:05:38 +0000

    remove example app

--- example/pages/[[...file]].php
+++
@@ -1,8 +0,0 @@
-<?php
-$class = new class {
-    #[\Darken\Attributes\RouteParam()]
-    public string $file;
-};
-?>
-<h1>Das ist wildcard file</h1>
-<h2><?= $class->file ?></h2>
--- example/pages/api/[[test]].php
+++
@@ -1,8 +0,0 @@
-<?php
-$class = new class {
-    #[\Darken\Attributes\RouteParam]
-    public string $test;
-}
-?>
-
-<h1>yes: <?= $class->test; ?></h1>
